5599204110223149780f5e0019c23c55e96cb5f3
bleek42 <brandonleek42@gmail.com>
2020-03-04 11:57:56 -0500

    hello world

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './HelloWorld.js';
 
 function App() {
   return (
---
+++ src/HelloWorld.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class HelloWorld extends React.Component () {
+    state = { 
+        who: 'world'
+};
+
+render(){
+    return (
+        <div className="HelloWorld">
+            <p> Hello, {this.state.who} </p>
+            <button onClick={() => this.setState( {who : 'world'} )}>World</button>
+            <button onClick={() => this.setState( {who : 'friend'} )}>Friend</button>
+            <button onClick={() => this.setState( {who : 'React'} )}>React</button>
+        </div>
+    )
+    }
+}
+
+export default HelloWorld;
